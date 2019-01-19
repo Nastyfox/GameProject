e565b9570f34f717fea51780b2456968deb00635
Nastyfox <julien.turbellier@gmail.com>
2019-01-20 00:01:33 +0100

    First commit for SDL library configuration

---
+++ Game/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "include/SDL.h"
+
+using namespace std;
+
+int main() {
+    if(SDL_Init(SDL_INIT_EVERYTHING) == -1)
+    {
+        cout << "Wrong" << SDL_GetError() << endl;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("SDL_Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
+
+    if(window == nullptr)
+    {
+        cout << "Wrong again" << endl;
+    }
+
+    SDL_Delay(2000);
+    SDL_Quit();
+
+    return 0;
+}
